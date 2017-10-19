dae5c72778e492964d352bc2546d3a26b9512a0c
notasheep <elisabeth.sanchezbeyrer@student.uni-augsburg.de>
2017-10-19 20:27:06 +0200

    des will net T_T

--- SUPERNATURAL <3/src/gui/HuntPickDia.java
+++ SUPERNATURAL <3/src/gui/HuntPickDia.java
@@ -13,24 +13,26 @@ public class HuntPickDia extends Dialog {
 	 */
 	private static final long serialVersionUID = 832190509465543828L;
     Button action = new Button("Kampf");
-    Panel po = new Panel();
-    Panel pu = new Panel();
-
+    Panel pr = new Panel();
+    Panel pl = new Panel();
+    List lsHunter = new List(1);
+    List lsDeamon = new List(1);
     public HuntPickDia(MainWindow m) {
         super(m ,"Neue Jagd beginnen");
         this.setLayout(new BorderLayout());
         this.setModal(true);
-        po.setLayout(new GridLayout(logik.CharacterContainer.instance().charconList.size()+1, 1));
-        this.add(po, BorderLayout.NORTH);
-        this.add(pu, BorderLayout.SOUTH);
+        pr.add(lsDeamon);
+        pl.add(lsHunter);
+        this.add(pr, BorderLayout.EAST);
+        this.add(pl, BorderLayout.WEST);
         for(character.Character c: logik.CharacterContainer.instance().charconList){
         	if(c.getType() == CharacterType.Hunter){
-        		po.add(new Checkbox(c.getName()));
+        		lsHunter.add(c.getName());
         	}
         	System.out.println(c.getType());
         	System.out.println(c.getName());
         }
-        pu.add(action);
+        this.add(action, BorderLayout.SOUTH);
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
